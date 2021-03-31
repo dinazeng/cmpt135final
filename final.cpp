2cf5a166f0231d74bcb41dab2875661a538fe19f
yka92 <yka92@sfu.ca>
2021-03-30 23:06:30 -0700

    created readFile
    Created a function to read in each entry in the magaList.txt file, separating each bit of information and storing into their respecrtive variables inside a variable of struct manga.

--- final.cpp
+++ final.cpp
@@ -13,19 +13,19 @@
 //#include <ncurse>
 using namespace std;
 
-struct mangaInformation {
+struct manga {
 
     string title;
-    vector<string> authors;
     vector<string> genres;
+    vector<string> authors;
     string status;
     string releaseDate;
 
 };
 
-vector<mangaInformation> readDatabase (string input){
+vector<manga> readFile (string input){
 
-    // Reads in the file containing manga information
+    // Reads in the file containing manga mangaInformation
     fstream inFile(input);
     // Checks if file name is valid
     if (inFile.fail()){
@@ -43,29 +43,85 @@ vector<mangaInformation> readDatabase (string input){
             cout << "What is the name of the file? (Your response requires case sensitivity and '.txt' at the end): ";
             string correctInput;
             cin >> correctInput;
-            readDatabase(correctInput);
+            readFile(correctInput);
         }
     }
 
     // Read in each input from the file
-    vector<mangaInformation> mangaList;
+    vector<manga> mangaList;
     while (true){
 
-        string listing;
-        mangaInformation information;
-        getline(inFile, listing);
+        string entry;
+        manga mangaInformation;
+        getline(inFile, entry);
 
         if (inFile.fail()){
             break;
         }
 
-        
+        // Getting title
+        string mangaTitle = entry.substr(0, entry.find(":"));
+        mangaInformation.title = mangaTitle; 
+        entry = entry.substr(entry.find(":") + 1); // Get rid of trailing :
 
+        // Getting Genres
+        string mangaGenres = entry.substr(0, entry.find(":"));
+        mangaGenres = mangaGenres.substr(1, mangaGenres.size() - 2); // Get rid of starting and trailing {}:
+        bool gotGenres = false;
+        while (gotGenres == false){
+            string mangaGenre;
+            if (mangaGenres.find(",") != -1){ // There are remaining genres
+                mangaGenre = mangaGenres.substr(0, mangaGenres.find(","));
+                mangaGenres = mangaGenres.substr(mangaGenres.find(",") + 2); // Get rid of comma and space
+                mangaInformation.genres.push_back(mangaGenre);
+            }
+            else { // Last genre in the list
+                mangaGenre = mangaGenres;
+                mangaInformation.genres.push_back(mangaGenre);
+                gotGenres = true;
+            } 
+        }
+        entry = entry.substr(entry.find(":") + 1);
+        
+        // Getting Authors
+        string mangaAuthors = entry.substr(0, entry.find(":"));
+        mangaAuthors = mangaAuthors.substr(1, mangaAuthors.size() - 2); // Get rid of starting and trailing {}:
+        bool gotAuthors = false;
+        while (gotAuthors == false){
+            string mangaAuthor;
+            if (mangaAuthors.find(",") != -1){ // There are remaining genres
+                mangaAuthor = mangaAuthors.substr(0, mangaAuthors.find(","));
+                mangaAuthors = mangaAuthors.substr(mangaAuthors.find(",") + 2); // Get rid of comma and space
+                mangaInformation.authors.push_back(mangaAuthor);
+            }
+            else { // Last genre in the list
+                mangaAuthor = mangaAuthors;
+                mangaInformation.authors.push_back(mangaAuthor);
+                gotAuthors = true;
+            } 
+        }
+        entry = entry.substr(entry.find(":") + 1);
+
+        // Getting Status
+        string mangaStatus = entry.substr(0, entry.find(":"));
+        mangaInformation.status = mangaStatus;
+        entry = entry.substr(entry.find(":") + 1);
+
+        // Getting Release Date
+        string mangaDate = entry;
+        mangaInformation.releaseDate = mangaDate;
+
+        // Append all this information to mangaList
+        mangaList.push_back(mangaInformation);
     }
+
+    inFile.close();
+    return mangaList;
 
 } 
 
 int main(){
     cout << "The beginning of the end." << endl;
+    vector<manga> mangaList = readFile("mangaList.txt");
 }
 
