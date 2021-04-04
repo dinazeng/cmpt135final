561b65e489d26b119beede95b57283806d918e15
Dina Zeng <dina.zeng22@gmail.com>
2021-04-03 21:27:06 -0700

    Update record.cpp

--- record.cpp
+++ record.cpp
@@ -2,17 +2,8 @@
 
 #include "cmpt_error.h"
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
 #include <vector>
 #include <string>
-#include <stdlib.h>
-#include <algorithm>
-#include <fstream>
-#include <cassert>
-#include <cmath>
-//#include <ncurse>
 using namespace std;
 
 class single_record{
