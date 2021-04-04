2c769cea8558bf9a97d128053fdd6e228ca8f667
Dina Zeng <dina.zeng22@gmail.com>
2021-04-03 21:23:19 -0700

    Add files via upload

---
+++ record.cpp
@@ -0,0 +1,51 @@
+//record class
+
+#include "cmpt_error.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+#include <algorithm>
+#include <fstream>
+#include <cassert>
+#include <cmath>
+//#include <ncurse>
+using namespace std;
+
+class single_record{
+    public:
+        single_record(string mangaName, vector<string> mangaAuthor, vector <string> mangaGenres, 
+                      bool mangaStatus, int releaseYear): name(mangaName), authors(mangaAuthor),
+                      genres(mangaGenres), isReleasing(mangaStatus), year(releaseYear){}
+
+        single_record(string mangaName, vector<string> mangaAuthor):
+            name(mangaName), authors(mangaAuthor){}
+
+        // Setters
+        void set_name(string newName){name = newName;}
+        void set_author(vector<string> newAuthor){authors = newAuthor;}
+        void set_genres(vector <string> newGenres) {genres = newGenres;}
+        void set_status (bool newStat){isReleasing = newStat;}
+        void set_year(int newYear){year = newYear;}
+    
+        // Getters
+        string getName() const { return name; }
+        vector<string> getAuthor() const { return authors; }
+        vector<string> getGenres() const { return genres; }
+        bool getStatus() const { return isReleasing; }
+        int getYear() const { return year; }
+
+        //Other functions
+        void add_genre(string newGenre){genres.push_back(newGenre);}
+        void add_author(string newAuthor){authors.push_back(newAuthor);}
+
+    private:
+        string name;
+        vector<string> authors;
+        vector <string> genres = {};
+        bool isReleasing = false;
+        int year = 0;
+};
