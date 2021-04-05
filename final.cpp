9c9da03e3cd308baf62a3dca778cae3134c5b5e8
yka92 <yka92@sfu.ca>
2021-04-04 21:18:26 -0700

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -1,18 +1,10 @@
 #include "cmpt_error.h"
 #include "final.h"
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <algorithm>
-#include <fstream>
-#include <cassert>
-#include <cmath>
+#include "menu.h"
 //#include <ncurse>
 
 using namespace std;
 
-int main() {}
+int main() {
+    menu finalProject;
+}
