0264095cd7b929934b5f03d6dc2bf5b9708ef6c2
yka92 <yka92@sfu.ca>
2021-04-04 17:58:37 -0700

    Made changes to how mangaList is stored, added functionality for deleting entries by name and year/year range
    database's readFile is now void, rather than returning mangaList it sets this->mangaList to the entries.
    
    added functionality for deleting entries. tested deleting entries by name, have yet to test for deleting entries by year and year range

--- database.h
+++ database.h
@@ -13,19 +13,34 @@ class database {
         database(){}
 
         // Reads in each entry in database.txt
-        vector<single_record> readFile (string input);
+        void readFile (string input);
 
+/* -------------------------- Searching for records ------------------------- */
         // Searching by name
-        single_record searchByName(string userInput);
-        
+        single_record searchByName (string name);
         // Searching by exact year
         vector<single_record> searchByYear (int year);
         // Searching by year range
         vector<single_record> searchByYear (int yearStart, int yearEnd);
 
+/* ------------------- Searching for and deleting records ------------------- */
+        // Searching by name
+        void deleteByName (string name);
+        // Searching by exact year
+        void deleteByYear (int year);
+        // Searching by year range
+        void deleteByYear (int yearStart, int yearEnd);
+
+        // Ask for confirmation on deleting record
+        bool deleteConfirmation(single_record manga);
+        void deleteConfirmationYear (vector<single_record> searchResults);
+
         // Getters
-        vector<single_record> getMangaList() const;
-        
+        vector<single_record> getMangaList() const {return mangaList;}
+
+        // Displaying a manga's information
+        void displayInformation(int index, single_record manga);
+
         // Deconstructor
         ~database();
 
